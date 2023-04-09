f55ff441ff624a52f9f69255fe27ae3e13354258
DieggoPacheco <dapachecopa@ufpso.edu.co>
2023-04-09 18:28:00 -0500

    se creo todo el modelo y se agrego el metodo para consumir y ambien crear un producto

--- src/main/java/com/parcial/procesos/controllers/ProductController.java
+++ src/main/java/com/parcial/procesos/controllers/ProductController.java
@@ -1,13 +1,55 @@
 package com.parcial.procesos.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.parcial.procesos.models.Product;
+import com.parcial.procesos.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ProductController {
 
-    @GetMapping(value = "/hola")
-    public String saludo(){
-        return "Api Base Funcionando";
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/products")
+    public List<Product> createAllProducts() {
+        RestTemplate restTemplate = new RestTemplate();
+        Product[] productosExternos = restTemplate.getForObject("https://fakestoreapi.com/products", Product[].class);
+        List<Product> productosLocales = new ArrayList<>();
+        for (Product productoExterno : productosExternos) {
+            Product productoLocal = new Product();
+            productoLocal.setTitle(productoExterno.getTitle());
+            productoLocal.setCategory(productoExterno.getCategory());
+            productoLocal.setPrice(productoExterno.getPrice());
+            productoLocal.setDescription(productoExterno.getDescription());
+            productoLocal.setImage(productoExterno.getImage());
+            productosLocales.add(productService.createProduct(productoLocal));
+        }
+        return productosLocales;
     }
+
+    @PostMapping(value = "/product")
+    public ResponseEntity createProduct(@RequestBody Product product){
+        Map response = new HashMap();
+        try{
+            response.put("message","Producto creado correctamente");
+            response.put("data",productService.createProduct(product));
+            return new ResponseEntity(response, HttpStatus.CREATED);
+        }catch (Exception e){
+            response.put("message","Error al crear producto");
+            response.put("data",e.getMessage());
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
---
+++ src/main/java/com/parcial/procesos/services/ProductServiceImpl.java
@@ -0,0 +1,38 @@
+package com.parcial.procesos.services;
+
+import com.parcial.procesos.models.Product;
+import com.parcial.procesos.repositorys.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAll(){
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id){
+        return productRepository.findById(id).get();
+    }
+
+    public Product updateProduct (Long id, Product product){
+        Product productDB = productRepository.findById(id).get();
+        productDB.setTitle(product.getTitle());
+        productDB.setPrice(product.getPrice());
+        productDB.setCategory(product.getCategory());
+        productDB.setDescription(product.getDescription());
+        productDB.setImage(product.getImage());
+        return productRepository.save(productDB);
+    }
+
+}
